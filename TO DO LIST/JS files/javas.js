26df2e96854e6e882c3308a92bfa3541e9d80ea3
Revathi Boni <143195098+sujirevathi@users.noreply.github.com>
2023-11-13 15:06:03 +0530

    Add files via upload

---
+++ TO DO LIST/JS files/javas.js
@@ -0,0 +1,24 @@
+document.querySelector("#push").onclick = function () {
+  //console.log("Click");
+  if(document.querySelector("#newtask input").value.length == 0) {
+      alert("Enter any task");
+  } else {
+      document.querySelector("#tasks").innerHTML += `
+      <div class="task">
+          <span id="taskname">
+          ${document.querySelector("#newtask input").value}
+          </span>
+          <button class="delete">
+        <i class="fa-solid fa-trash-can"></i>
+      </button>
+      </div>
+      `;
+  var current_tasks = document.querySelectorAll(".delete");
+  console.log(typeof current_tasks);
+  for (var i = 0; i < current_tasks.length; i++) {
+    current_tasks[i].onclick = function () {
+      this.parentNode.remove();
+    };
+  }
+}
+};
